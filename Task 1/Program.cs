022c86c3da5f556f73afce19ee750407232bc19c
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 01:47:18 +0100

    Task 1 completed

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 namespace Task_1 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
+            String readLine;
+            int num;
+            string evenOdd;
 
+            while (true) {
+                Console.Write("Enter a Number to see it its even or odd ");
+                readLine = Console.ReadLine();
+                if (!int.TryParse(readLine.ToString(), out num)) continue;
+                evenOdd = (num % 2 == 0)? "even" : "odd";
+                Console.WriteLine($"{num} is {evenOdd}");
+            }
+        }
     }
 }
