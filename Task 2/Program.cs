ff8542d1b51583e624a94f4c4dcb84d2e683e66c
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 02:01:50 +0100

    task 2 completed

---
+++ Task 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2 {
+    internal class Program {
+        static void Main(string[] args) {
+            // basic variable decleration
+            String readA;
+            String readB;
+            float numA;
+            float numB;
+
+            while (true) {
+
+                // getting user input
+                Console.WriteLine("Enter A and B to calculate (a*b) / (a+b)");
+                Console.Write("A: ");
+                readA = Console.ReadLine();
+                Console.Write("B: ");
+                readB = Console.ReadLine();
+
+                // guardian clauses for A and B to be numerical
+                if (!float.TryParse(readA.ToString(), out numA)) continue;
+                if (!float.TryParse(readB.ToString(), out numB)) continue;
+
+                Console.WriteLine($"Result of the calculation: {(float)(numA * numB) / (float)(numA + numB)}");
+            }
+        }
+    }
+}
