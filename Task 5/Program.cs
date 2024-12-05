59e46d1972199ec6c084e4a41002537d47d52061
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 02:58:47 +0100

    Task 5 completed

---
+++ Task 5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_5 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            char[] operators = { '+', '-', '*', '/' };
+            float numA;
+            float numB;
+            float result = 0;
+            char op;
+
+            while (true) {
+
+                // getting user input and guardian clauses for miss input
+                Console.WriteLine("Enter two numbers A and B followed by a operator(+,-,*,/) you want to apply to the two numbers (A x B): ");
+                Console.Write("A: ");
+                if (!float.TryParse(Console.ReadLine(), out numA)) continue;
+                Console.Write("B: ");
+                if (!float.TryParse(Console.ReadLine(), out numB)) continue;
+                Console.Write("Operator: ");
+                op = Console.ReadKey().KeyChar;
+                if (!operators.Contains(op)) continue;
+                Console.WriteLine();
+
+                switch (op) {
+                    case '+':
+                        result = numA + numB;
+                        break;
+                    case '-':
+                        result = numA - numB;
+                        break;
+                    case '*':
+                        result = numA * numB;
+                        break;
+                    case '/':
+                        result = numA / numB;
+                        break;
+                }
+
+                Console.WriteLine($"Result: {result}");
+            }
+
+        }
+    }
+}
