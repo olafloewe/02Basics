a670a817aa690d33bf9d95aa2a31e489bda63ea2
Olaf Loewe <olafloewe95@gmail.com>
2024-12-05 02:57:38 +0100

    Task 4 completed

--- Task 4/Program.cs
+++ Task 4/Program.cs
@@ -8,18 +8,38 @@ namespace Task_4 {
             char[] operators = { '+', '-', '*', '/'};
             float numA;
             float numB;
-            char operator;
+            float result = 0;
+            char op;
 
             while (true) {
 
                 // getting user input and guardian clauses for miss input
                 Console.WriteLine("Enter two numbers A and B followed by a operator(+,-,*,/) you want to apply to the two numbers (A x B): ");
-                if (float.TryParse(Console.Read("A: "), out numA)) continue;
-                if (float.TryParse(Console.Read("B: "), out numB)) continue;
-                operator = Console.ReadKey().KeyChar;
-                if (!operators.Contains(operators)) continue;
+                Console.Write("A: ");
+                if (!float.TryParse(Console.ReadLine(), out numA)) continue;
+                Console.Write("B: ");
+                if (!float.TryParse(Console.ReadLine(), out numB)) continue;
+                Console.Write("Operator: ");
+                op = Console.ReadKey().KeyChar;
+                if (!operators.Contains(op)) continue;
+                Console.WriteLine();
 
-                Console.WriteLine($"{numA} {numB} {operator}");
+                switch (op) {
+                    case '+':
+                        result = numA + numB;
+                        break;
+                    case '-':
+                        result = numA - numB;
+                        break;
+                    case '*':
+                        result = numA * numB;
+                        break;
+                    case '/':
+                        result = numA / numB;
+                        break;
+                }
+
+                Console.WriteLine($"Result: {result}");
             }
 
         }
